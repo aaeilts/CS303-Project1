58716effffb00d57770f55d911184c4dcfce67bd
aaeilts <60226103+aaeilts@users.noreply.github.com>
2020-07-07 16:46:19 -0500

    Some member functions

---
+++ CS303-Project1/Evaluator.cpp
@@ -0,0 +1,18 @@
+#include <stack>
+#include "Evaluator.h"
+using namespace std;
+
+
+
+stack <int> Evaluator::GetOperand() {
+	return this->operands;
+}
+
+stack <string> Evaluator::GetOperator() {
+	return this->operators;
+}
+
+void CompareOperator() {
+
+}
+
--- CS303-Project1/Evaluator.h
+++ CS303-Project1/Evaluator.h
@@ -1,11 +1,18 @@
 #include <stack>
+#include <string>
+#include <vector>
+
 using namespace std;
 
 class Evaluator{
 public:
-
+	Evaluator();
+	stack <int> GetOperand();
+	stack <string> GetOperator();
+	void CompareOperator();
 
 private:
 	stack <int> operands;
 	stack <string> operators;
+	const vector <int>  precenedence;
 };
